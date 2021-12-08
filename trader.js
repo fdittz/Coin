0a67351cdade98634025589170373c8935b47fff
fdittz <fdittz@gmail.com>
2021-12-08 11:03:41 -0300

    progress bar

--- trader.js
+++ trader.js
@@ -1,5 +1,6 @@
 const Config = require('./config.js')
 const WebSocket = require('ws');
+const CliProgress = require('cli-progress');
 const Exchange = require('./exchanges/binance');
 
 const getDate = require('./util.js');
@@ -26,6 +27,7 @@ module.exports = class Trader  {
         this.lastBnbValue = 0;      // Current BNB value in the user-defined currency, used to calculate commission
         this.commission = 0;         // Total commission (in user-defined currency) paid
         this.awaitingTrade = false;
+        this.bar = null;
     }
 
     setConfig() {
@@ -68,6 +70,17 @@ module.exports = class Trader  {
         return this.config.type == "SHORT" && price <= this.targetPrice
     }
 
+    updateBar(price) {
+        if (this.config.type == "SHORT") {
+            this.bar.update(price - this.targetPrice);
+            return price - this.targetPrice;
+        }
+        else if (this.config.type == "LONG") {
+            this.bar.update(price - this.safetyPrice);
+            return price - this.safetyPrice;
+        }
+    }
+
     async placeShortOrder(initialBase, currentPrice) {
         const CONFIG = this.config;
         try {
@@ -95,6 +108,33 @@ module.exports = class Trader  {
             console.log(getDate(), this.safetyStep > 0 ? `[Safety Step ${this.safetyStep}]` : "[Base Order]", `Holding ${this.amountIn} ${CONFIG.quote} at an avg. price of ${this.avgPrice} ${CONFIG.quote}, total spent ${this.totalAllocated} ${CONFIG.base}`);
             console.log(getDate(), this.safetyStep > 0 ? `[Safety Step ${this.safetyStep}]` : "[Base Order]", `Target Take Profit Price: ${this.targetPrice} ${CONFIG.quote}`);
             console.log(getDate(), this.safetyStep > 0 ? `[Safety Step ${this.safetyStep}]` : "[Base Order]", `Safety Order Price: ${this.safetyPrice}`);
+            
+            var self = this
+            this.bar = new CliProgress.SingleBar({
+                format: `Last: \x1b[33m{value}\x1b[0m | \x1b[33m{percentage}%\x1b[0m >> \x1b[31m${this.safetyPrice}\x1b[0m {bar} \x1b[32m${this.targetPrice}\x1b[0m`,
+                formatValue: function(v, options, type) {
+                    if (options.autopadding !== true){
+                        if (type == 'percentage')
+                            return v;
+                        else
+                            return self.targetPrice + v
+                    }
+                    function autopadding(value, length){
+                        return (options.autopaddingChar + value).slice(-length);
+                    }
+                    switch (type){
+                        case 'percentage':
+                            return autopadding(v, 3);
+                
+                        default: 
+                            return v;
+                    }
+                },
+            }, CliProgress.Presets.shades_classic);
+            let range = (this.safetyPrice - this.targetPrice);
+            this.bar.start(range,0);
+            if (this.currentPrice > 0)
+                this.updateBar(currentPrice);
         }
         catch (err) {
             console.log(getDate(),"Error placing base order")
@@ -130,6 +170,33 @@ module.exports = class Trader  {
             console.log(getDate(), this.safetyStep > 0 ? `[Safety Step ${this.safetyStep}]` : "[Base Order]", `Holding ${this.amountIn} ${CONFIG.base} at an avg. price of ${this.avgPrice} ${CONFIG.quote}, total spent ${this.totalAllocated} ${CONFIG.quote}`);
             console.log(getDate(), this.safetyStep > 0 ? `[Safety Step ${this.safetyStep}]` : "[Base Order]", `Target Take Profit Price: ${this.targetPrice} ${CONFIG.quote}`);
             console.log(getDate(), this.safetyStep > 0 ? `[Safety Step ${this.safetyStep}]` : "[Base Order]", `Safety Order Price: ${this.safetyPrice}`);
+
+            var self = this
+            this.bar = new CliProgress.SingleBar({
+                format: `Last: \x1b[33m{value}\x1b[0m | \x1b[33m{percentage}%\x1b[0m >> \x1b[31m${this.targetPrice}\x1b[0m {bar} \x1b[32m${this.safetyPrice}\x1b[0m`,
+                formatValue: function(v, options, type) {
+                    if (options.autopadding !== true){
+                        if (type == 'percentage')
+                            return v;
+                        else
+                            return self.targetPrice + v
+                    }
+                    function autopadding(value, length){
+                        return (options.autopaddingChar + value).slice(-length);
+                    }                
+                    switch (type){
+                        case 'percentage':
+                            return autopadding(v, 3);
+                
+                        default: 
+                            return v;
+                    }
+                },
+            }, CliProgress.Presets.shades_classic);
+            let range = (this.targetPrice - this.safetyPrice);
+            this.bar.start(range,0);
+            if (this.currentPrice > 0)
+                this.updateBar(currentPrice);
         }
         catch (err) {
             console.log(getDate(),"Error placing base order");
@@ -146,6 +213,9 @@ module.exports = class Trader  {
             var obj = JSON.parse(event.data);
             if (obj.e == "trade" && this.lastBnbValue > 0 && !this.awaitingTrade) {                
                 obj.p = parseFloat(obj.p);
+                if (this.bar) {                    
+                    this.updateBar(obj.p);
+                }
                 if (!this.active) {
                     this.active = true;
                     this.awaitingTrade = true;                    
@@ -167,6 +237,7 @@ module.exports = class Trader  {
                     this.awaitingTrade = true; // Sets this flag so no order is placed until the safety order creation is done  
                     if (this.safetyPrice && ( this.isPlaceLongSafetyOrder(obj.p) || this.isPlaceShortSafetyOrder(obj.p))) {                // Price has fallen (LONG) or risen(SHORT) beyond the defined % threshold, will place a safety market order
                         if (this.safetyStep < CONFIG.numSafetyOrders) {                                                                    // Still have safety orders available                                                                                                                    
+                            process.stdout.write("\r\x1b[K")
                             console.log(getDate(), this.safetyStep > 0 ? `[Safety Step ${this.safetyStep}]` : "[Base Order]", `Unable to reach current target, placing Safety Order at ${this.safetyPrice}`);
                             this.safetyStep++;                                                                                             // Advances safety order step
                             let nextStepStartQuantity = CONFIG.safetyOrderSize * Math.pow(CONFIG.volumeScaling, this.safetyStep - 1);      // Defines the quantity of base or quote order to be used on the next safety order (see user-defined configurations for scaling)
@@ -194,7 +265,7 @@ module.exports = class Trader  {
                         }
                     }
                     else if (this.targetPrice && ( this.isPlaceLongTakeProfitOrder(obj.p) || this.isPlaceShortTakeProfitOrder(obj.p) ) ) { // Price has risen (LONG) or fallen(SHORT) beyond the defined % threshold, will place a take profit market order
-
+                        
                         let result = {};
                         try {
                             if (CONFIG.type == "LONG") 
@@ -207,7 +278,7 @@ module.exports = class Trader  {
                             return;
                         }
                         
-                        
+                        process.stdout.write("\r\x1b[K")
                         if (result.orderId) {
                             result.price = result.cummulativeQuoteQty / result.executedQty;                                                           // Gets the actual price of the resulting take profit order
                             if (this.debug)
